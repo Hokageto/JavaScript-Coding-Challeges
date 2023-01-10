2053ce85eb8781098cb9bcdc1ea690562217d0a2
Hokageto <didorhristozov@gmail.com>
2023-01-10 14:22:19 +0200

    test

--- js-coding-challenge-3/solution.js
+++
@@ -1,9 +0,0 @@
-function removeCharacter(str, charPos) {
-  console.log(
-    str.substring(0, charPos - 1) + str.substring(charPos, str.length)
-  );
-}
-
-removeCharacter("Example", 0);
-removeCharacter("Example", 3);
-removeCharacter("Example", 5);
--- js-coding-challenge-4/solution.js
+++
@@ -1,7 +0,0 @@
-function checkStringStart(str) {
-  return str.startsWith("Java");
-}
-
-console.log(checkStringStart("JavaScript"));
-console.log(checkStringStart("Java"));
-console.log(checkStringStart("Python"));
